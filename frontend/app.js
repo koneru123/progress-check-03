b49d97a0e40413537f4e37495759e58ece6b2ef6
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-02 09:44:24 -0800

    Clicking on background colored boxes should add text

--- frontend/app.js
+++ frontend/app.js
@@ -4,11 +4,10 @@ $(document).ready(function(){
   // do not add any more event listeners
   // do not add a .on or .click to the code.
   // do not pass go , do not collect $200
-  $('.color1').on('click', function(event){
+  $('#colors').on('click', function(){
     var curDate = Date.now();
-    var $target = $( event.target) ;
-    var color = $target.css('background-color');
-    $('#times').append(`<div style='color:${color}'>${curDate}</div>`)
+      var $target = $(event.target) ;
+      var color = $target.css('background-color');
+      $('#times').append(`<div style='color:${color}'>${curDate}</div>`)
   });
-
 });
